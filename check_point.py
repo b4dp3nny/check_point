25e8f46261320574c5951f5a8f58e39834f32218
Richard Lewis. Kale <rk2025@internal.blackwall.co.nz>
2017-11-13 13:46:36 +1300

    __init__ added

--- check_point.py
+++ check_point.py
@@ -2,10 +2,13 @@ import json
 import requests
 
 class Check_Point(object):
-    __init__(self,host,port,username,password,command={}):
-       self.host = host
-       self.port = port
-       self.username = username
-       self.password = password
-       self.command = json.dumps(command)
-       
+   def __init__(self,host,port,username,password):
+      self.host = host
+      self.port = port
+      self.username = username
+      self.password = password
+      self.url_base = "{h}:{p}/web_api"
+
+
+
+                             
