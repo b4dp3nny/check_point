f689c004f1a2b093b510611bb2c78c25a4a1363c
Richard Lewis. Kale <rk2025@internal.blackwall.co.nz>
2017-11-14 09:32:56 +1300

    added login function

--- check_point.py
+++ check_point.py
@@ -1,14 +1,21 @@
 import json
 import requests
+import urllib3
 
 class Check_Point(object):
    def __init__(self,host,port,username,password):
+      urllib3.disable_warnings()
       self.host = host
       self.port = port
       self.username = username
       self.password = password
-      self.url_base = "{h}:{p}/web_api"
+      self.url_base = "https://{h}:{p}/web_api".format(h=self.host,p=self.port)
+      self.login()
 
-
-
-                             
+   def login(self):
+      payload = { 'user':self.username,'password':self.password }
+      url = "{u}/login".format(u=self.url_base)
+      headers = { 'Content-Type':'application/json' }
+      req = requests.post(url,json.dumps(payload),headers=headers,verify=False)
+      req = req.json()
+      print(req["sid"])
